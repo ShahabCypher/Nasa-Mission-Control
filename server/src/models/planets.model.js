2e0a7b2a72ca30723d6e6fa9f7eb0a200160a626
Shahab Cypher <shahabcypher@gmail.com>
2025-08-13 16:26:46 +0330

    getting planets data from csv file

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,39 @@
-const planets = [];
+const { parse } = require("csv-parse");
+const fs = require("fs");
 
-module.exports = planets;
+const habitablePlanets = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+const loadPlanetsData = () => {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream("data/kepler_data.csv")
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on("error", (err) => reject(err))
+      .on("end", () => {
+        resolve();
+      });
+  });
+};
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,18 @@
 const http = require("http");
 require("dotenv").config();
 
+const { loadPlanetsData } = require("./models/planets.model");
 const app = require("./app");
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+startServer();
